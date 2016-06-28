02623d27167495f04923fe8e6eee04473d1f93a0
Alexis Mifsud <alexis.mifsud@gmail.com>
2016-06-28 12:08:56 +0200

    frequency and amplitude in a list for playTone method and adding fourrier_series

--- interface_aport.py
+++ interface_aport.py
@@ -9,12 +9,14 @@ Created on Mon Jun 27 12:05:09 2016
 import pyaudio
 import wave
 import numpy as np
+import scipy.fftpack as fftp
+import matplotlib.pyplot as plt
 
 class InterfaceAport(object):
     
     def __init__(self):
 
-        # Audio parameters
+        # Stream parameters
         self.bitrate_ = 44100
         self.channels_ = 1
 
@@ -22,6 +24,10 @@ class InterfaceAport(object):
         self.p_ = pyaudio.PyAudio()
         self.stream_ = None
         self.createStream()
+        
+    def __del__(self):
+        self.p_.terminate()
+        self.stream_.close()
         
     @property
     def bitrate(self):
@@ -38,11 +44,7 @@ class InterfaceAport(object):
     def channels(self, x):
         self.channels_=x
         self.createStream()
-            
-    def __del__(self):
-        self.p_.terminate()
-        self.stream_.close()
-        
+                  
     def createStream(self):
         if self.stream_ != None:
             self.stream_.close()
@@ -52,15 +54,19 @@ class InterfaceAport(object):
             channels = self.channels_, 
             rate = self.bitrate_, 
             output = True)
-    
-    def playTone(self,f,amp,duration):
-        numberofframes = int(self.bitrate_ * duration)
+       
+    def playTone(self,s,duration):
         
-        data = ''    
+        f=s[0]
+        amp=s[1]
+        
+        numberofframes = int(self.bitrate_ * duration)
+        data = ''  
+        
         for x in xrange(numberofframes):
             data = data+chr(int(amp*np.sin(x/((self.bitrate_/f)/3.14))*127+128))    
 
-        self.stream_.write(data)  
+        self.stream_.write(data)
               
     def playWave(self, fileName):
         
@@ -79,20 +85,36 @@ class InterfaceAport(object):
             data = wf.readframes(chunk)
             
         self.createStream()
+        
+
+    def fourier_series(self, x, y, wn=0, n=None):
+        # get FFT
+        myfft = fftp.fft(y, n)
+        # kill higher freqs above wavenumber wn
+        myfft[wn:-wn] = 0
+        # make new series
+        y2 = fftp.ifft(myfft).real
+        
+        plt.figure(num=None)
+        plt.plot(x, y, x, y2)
+        plt.show()
 
 if __name__ == "__main__":
 
     interface=InterfaceAport()
     
     #interface.playWave("ressources/COW_1.WAV")
-    #interface.playTone(440,0.5,3) # f(Hz), rate of amplitude, duration (s)
+    #interface.playTone([440,0.5],3) # f(Hz), rate of amplitude, duration (s)
     
-    print interface.stream_.__dict__
-    interface.bitrate=22000
-    interface.channels=2
-    print interface.stream_.__dict__
+#    print interface.stream_.__dict__
+#    interface.bitrate=22000
+#    interface.channels=2
+#    print interface.stream_.__dict__
     
     #del interface
     
+    x = np.array([float(i) for i in range(0,360)])
+    y = np.sin(2*np.pi/360*x) + np.sin(2*2*np.pi/360*x) + 5
 
+    interface.fourier_series(x, y, 3, 360)
     
