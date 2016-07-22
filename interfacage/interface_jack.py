66257e8f5c54bab3cdfd00a7f5703780635135ac
Alexis Mifsud <alexis.mifsud@gmail.com>
2016-07-23 01:46:43 +0200

    moving connection to gmidimonitor in class and adding disconnection

--- interfacage/interface_jack.py
+++ interfacage/interface_jack.py
@@ -11,15 +11,27 @@ class InterfaceJack(object):
     
     def __init__(self):
         self.client = jack.Client("jackClient")
+        self.client.activate()
+        
+    def __del__(self):
+        self.client.deactivate()
+        self.client.close()
+        
+    def connectToGmidimonitor (self):
+        # WE need to launch a2j -e and gmidimonitor before
+        if self.client.get_all_connections("a2j:MIDI monitor [132] (playback): midi in") == []:
+            self.client.connect("a2j:XP MIDI mate [24] (capture): XP MIDI mate MIDI 1", 
+                                 "a2j:MIDI monitor [132] (playback): midi in")
+                             
+    def disconnectFromGmidimonitor(self):
+        self.client.disconnect("a2j:XP MIDI mate [24] (capture): XP MIDI mate MIDI 1", 
+                             "a2j:MIDI monitor [132] (playback): midi in")
         
 if __name__ == "__main__":
     
     interface = InterfaceJack()
+    interface.connectToGmidimonitor()
     
-    print interface.client.get_ports()
-        
-    interface.client.activate()
-    
-    # WE need to launch a2j -e and gmidimonitor before
-    interface.client.connect("a2j:XP MIDI mate [24] (capture): XP MIDI mate MIDI 1", 
-                             "a2j:MIDI monitor [132] (playback): midi in")
+    interface.disconnectFromGmidimonitor()
+    del interface
+    #print interface.client.get_ports()
