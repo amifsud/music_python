1033cea8fd34ca169c63c574c53d57eb2475a979
Alexis Mifsud <alexis.mifsud@gmail.com>
2016-08-20 18:31:16 +0200

    adding scale.py

---
+++ music/scale.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 20 18:22:12 2016
+
+@author: alexis
+"""
+
+class Scale(object):
+    def __init__(self):
+        self.tonality = 'c'
+        
+        
+if __name__ == "__main__":
+    
+    scale = Scale()
+    print scale.tonality
