5a90e2893e98459bc121a9597d68c07092744a7c
Alexis Mifsud <alexis.mifsud@gmail.com>
2016-08-21 18:27:34 +0200

    adding abstractSheet

---
+++ music/abstractSheet.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  6 19:56:50 2016
+
+@author: alexis
+"""
+
+from note import Note
+
+class AbstractSheet(object):
+    
+    def __init__(self):
+        
+        self.notes_ = ()
+        self.lastAddedNoteIndex_ = None
+        self.lastPlayedNoteIndex_ = 0
+        self.lastTimeDiv_ = None
+        
+    @property
+    def lastAddedNote(self):
+        if self.lastAddedNoteIndex_ != None:
+            return self.notes_[self.lastAddedNoteIndex_]
+        else:
+            return Note(None, None)
+            
+    @property
+    def lastPlayedNote(self):           
+        if self.lastPlayedNoteIndex_ <= self.lastAddedNoteIndex_:
+            note = self.notes_[self.lastPlayedNoteIndex_]
+            self.lastPlayedNoteIndex_ += 1
+        else:
+            if self.lastPlayedNoteIndex_ != 0:
+                print "No more note"
+                note = Note(None, None)
+            else:
+                print "Still no note"
+                note = Note(None, None)
+
+        return note
+
+    def addNote(self, height, timeDiv):
+        if self.lastAddedNoteIndex_ !=None:
+            self.lastAddedNoteIndex_+=1
+        else:
+            self.lastAddedNoteIndex_ = 0
+        self.notes_ += (Note(height,timeDiv),)
+        
+    def addEnd(self):
+        if self.lastAddedNoteIndex_ !=None:
+            self.lastAddedNoteIndex_+=1
+        else:
+            self.lastAddedNoteIndex_ = 0
+        self.notes_ += ('end',)
+        
+if __name__ == "__main__":
+    a=1
--- music/sheet.py
+++ music/sheet.py
@@ -5,52 +5,13 @@ Created on Wed Jul  6 19:56:50 2016
 @author: alexis
 """
 
-from note import Note
+from abstractSheet import AbstractSheet
 
-class Sheet(object):
+class Sheet(AbstractSheet):
     
     def __init__(self,):
-        
-        self.notes_ = ()
-        self.lastAddedNoteIndex_ = None
-        self.lastPlayedNoteIndex_ = 0
-        self.lastTimeDiv_ = None
-        
-    @property
-    def lastAddedNote(self):
-        if self.lastAddedNoteIndex_ != None:
-            return self.notes_[self.lastAddedNoteIndex_]
-        else:
-            return Note(None, None)
-            
-    @property
-    def lastPlayedNote(self):           
-        if self.lastPlayedNoteIndex_ <= self.lastAddedNoteIndex_:
-            note = self.notes_[self.lastPlayedNoteIndex_]
-            self.lastPlayedNoteIndex_ += 1
-        else:
-            if self.lastPlayedNoteIndex_ != 0:
-                print "No more note"
-                note = Note(None, None)
-            else:
-                print "Still no note"
-                note = Note(None, None)
+        AbstractSheet.__init__(self)        
 
-        return note
-
-    def addNote(self, height, timeDiv):
-        if self.lastAddedNoteIndex_ !=None:
-            self.lastAddedNoteIndex_+=1
-        else:
-            self.lastAddedNoteIndex_ = 0
-        self.notes_ += (Note(height,timeDiv),)
-        
-    def addEnd(self):
-        if self.lastAddedNoteIndex_ !=None:
-            self.lastAddedNoteIndex_+=1
-        else:
-            self.lastAddedNoteIndex_ = 0
-        self.notes_ += ('end',)
         
 if __name__ == "__main__":
     
