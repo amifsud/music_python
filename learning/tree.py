e788125468528bab64c840e4bd06abc74fdf0a94
Alexis Mifsud <alexis.mifsud@gmail.com>
2017-11-12 18:19:12 +0100

    adding parseNote in Tree

--- learning/tree.py
+++ learning/tree.py
@@ -44,6 +44,7 @@ class Tree(object):
         self.heads = {}
         
         self.listOfNodes = []
+        self.historySize = 4
         
     def addVertex(self, obj, p=None):
         self.tree.add_vertex()
@@ -59,31 +60,37 @@ class Tree(object):
         return n
                
     def addContinuation(self,SIn,c,p=None):
-        S = list(SIn)
-        if p == None:
-            S.reverse()
-            s=S.pop(0)
-            if s in self.heads:
-                p = self.heads[s]
-            else:
-                p = self.createNode(s)
-        else: 
-            s=S.pop(0)
-            if s in p.childs:
-                p = p.childs[s]
-            else:
-                p = self.createNode(s,p)
-            
-        p.addContinuation(c)        
-        if len(S)>0 : self.addContinuation(S,c,p)
+        if len(SIn) > 0:
+            S = list(SIn)
+            if p == None:
+                S.reverse()
+                s=S.pop(0)
+                if s in self.heads:
+                    p = self.heads[s]
+                else:
+                    p = self.createNode(s)
+            else: 
+                s=S.pop(0)
+                if s in p.childs:
+                    p = p.childs[s]
+                else:
+                    p = self.createNode(s,p)
+            p.addContinuation(c)        
+            if len(S)>0 : self.addContinuation(S,c,p)
         
-    def parseSequence (self,SIn):
+    def parseNotesSequence (self,SIn):
         S = list(SIn)
         while len(S) > 1:
             currentContinuation = len(self.listOfNodes) + len(S)
             S.pop(len(S)-1)
             self.addContinuation(S,currentContinuation)  
         self.listOfNodes += SIn
+        
+    def parseNote(self,note):
+        self.listOfNodes += [note]
+        size = len(self.listOfNodes)
+        subList = self.listOfNodes[max(0,size-self.historySize-1):max(0,size-1)]
+        self.addContinuation(subList, size)
         
     def display(self):
         layout = self.tree.layout('kk')
@@ -93,8 +100,14 @@ class Tree(object):
 if __name__ == "__main__":
     
     T = Tree()
-    T.parseSequence(['A','B','C','D'])
-    T.parseSequence(['A','B','B','C'])
+    T.parseNotesSequence(['A','B','C','D'])
+    #T.parseNotesSequence(['A','B','B','C'])
     T.display()
+    
+    T1 = Tree()
+    l=['A','B','C','D']
+    for n in l:
+        T1.parseNote(n)
+    T1.display()
     
     
