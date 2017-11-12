1c72f091a7363b7fcd97c4dd85c24f6a64f30168
Alexis Mifsud <alexis.mifsud@gmail.com>
2017-11-12 17:43:14 +0100

    adding Tree displaying and simplification of Tree class

--- learning/tree.py
+++ learning/tree.py
@@ -5,12 +5,15 @@ Created on Mon Aug 22 01:08:11 2016
 @author: alexis
 """
 
-from ete2 import Tree
+import plotly.plotly as py
+import plotly.graph_objs as go
+from igraph import Graph, plot
 
 class Node(object):
-    def __init__(self,name,p=None):
+    def __init__(self,name,index,p=None):
         
         self.name = name
+        self.index = index
         self.continuations = []
         
         self.parent = None
@@ -35,59 +38,63 @@ class Node(object):
 class Tree(object):
     def __init__(self, ):
         
+        self.tree = Graph()
+        
         self.nodes = []
         self.heads = {}
         
-        self.totalSize = 0
-        self.currentContinuation = None
+        self.listOfNodes = []
+        
+    def addVertex(self, obj, p=None):
+        self.tree.add_vertex()
+        self.tree.vs[len(self.tree.vs)-1]['obj']=obj
+        self.tree.vs[len(self.tree.vs)-1]['name']=obj.name
+        if p!=None : self.tree.add_edge(p.index,obj.index)
         
     def createNode(self,s,p=None):
-        n = Node(s,p)       
+        n = Node(s,len(self.nodes),p)       
         self.nodes.append(n)
         if p == None: self.heads[s] = n
+        self.addVertex(n,p)
         return n
                
-    def parseSubSequence (self,S,p=None):
-        if p != None: 
-            s=S.pop(0)
-            if s in p.childs:
-                p = p.childs[s]
-            else:
-                p = self.createNode(s,p)               
-        else:
+    def addContinuation(self,SIn,c,p=None):
+        S = list(SIn)
+        if p == None:
             S.reverse()
             s=S.pop(0)
             if s in self.heads:
                 p = self.heads[s]
             else:
                 p = self.createNode(s)
+        else: 
+            s=S.pop(0)
+            if s in p.childs:
+                p = p.childs[s]
+            else:
+                p = self.createNode(s,p)
             
-        p.addContinuation(self.currentContinuation)        
-        if len(S)>0 : self.parseSubSequence(S,p)
+        p.addContinuation(c)        
+        if len(S)>0 : self.addContinuation(S,c,p)
         
-    def parseSequence (self,S):
-        S1 = list(S)
-        S2 = list(S)
-        lS1 = len(S1)
-        while len(S1) > 0:
-            self.currentContinuation = self.totalSize + len(S1) + 1
-            if self.currentContinuation > self.totalSize+lS1: self.currentContinuation = None
-            self.parseSubSequence(S1)
-            S2.pop(len(S2)-1)
-            S1 = list(S2)
-        self.totalSize += lS1
+    def parseSequence (self,SIn):
+        S = list(SIn)
+        while len(S) > 1:
+            currentContinuation = len(self.listOfNodes) + len(S)
+            S.pop(len(S)-1)
+            self.addContinuation(S,currentContinuation)  
+        self.listOfNodes += SIn
         
-#    def display(self):
-#        t = "("
-#        for n in self.heads:
-#            t+= self.heads[n].name + 
-#        t += ";)"
-        
+    def display(self):
+        layout = self.tree.layout('kk')
+        p = plot(self.tree, layout = layout)
+        p.show()
 
 if __name__ == "__main__":
     
     T = Tree()
     T.parseSequence(['A','B','C','D'])
     T.parseSequence(['A','B','B','C'])
+    T.display()
     
     
